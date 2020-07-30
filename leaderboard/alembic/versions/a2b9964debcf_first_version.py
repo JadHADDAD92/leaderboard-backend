6069a9147bdf351ea3f465c163c90c385f1a4eff
Jad Haddad <jad.haddad@zto-technology.com>
2020-07-30 16:01:55 +0200

    Feature: integrate alembic db migration system

---
+++ leaderboard/alembic/versions/a2b9964debcf_first_version.py
@@ -0,0 +1,24 @@
+"""first version
+
+Revision ID: a2b9964debcf
+Revises: 
+Create Date: 2020-07-30 14:38:00.381457
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'a2b9964debcf'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
